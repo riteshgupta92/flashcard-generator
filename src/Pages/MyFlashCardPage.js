62da99e9698d85c22ce7335d522a93e6008adca9
riteshgupta92 <riteshgupta.shyamagro@gmail.com>
2024-07-04 17:27:31 +0530

    some change on code

--- src/Pages/MyFlashCardPage.js
+++ src/Pages/MyFlashCardPage.js
@@ -8,21 +8,34 @@ import "react-toastify/dist/ReactToastify.css";
 import DeleteModal from "../Components/DeleteModel";
 
 const MyFlashcardsPage = () => {
+  // state to control the visibility of the detete confirmation modal.
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  // state to store the flashcard data fetched from local storage.
   const [flashCardData, setFlashCardData] = useState([]);
+
+  // state to store the flashcard items that is selected from deletion.
   const [delClickedItem, setDelClickedItem] = useState(null);
+
+  // state to control how many flashcard display at a time.
   const [showCard, setShowCard] = useState(6);
+
+  // hooks from react-router-dom to naviagte between the pages.
   const navigate = useNavigate();
 
+  // useEffect hooks to fetch flashcard data from localstorage on component did mount.
   useEffect(() => {
-    const storedFlashCards = JSON.parse(localStorage.getItem("flashCards")) || [];
+    const storedFlashCards =
+      JSON.parse(localStorage.getItem("flashCards")) || [];
     setFlashCardData(storedFlashCards);
   }, []);
 
+  // function to  navigate to the flashcard details page, passing the selected flashcard item as state.
   const handleViewCardsClick = (elem) => {
     navigate("/flashCardDetails", { state: elem });
   };
 
+  // function to set the item to be deleted and show the delete confimation modal.
   const deleteFlashCard = (delClickedItem) => {
     setDelClickedItem(delClickedItem);
     setShowDeleteModal(true);
@@ -38,11 +51,18 @@ const MyFlashcardsPage = () => {
           setFlashCardData={setFlashCardData}
           delClickedItem={delClickedItem}
         />
+        {/* Render the toast notification container*/}
         <ToastContainer />
         <div className="absolute pr-10 overflow-visible text-sm font-bold text-right text-gray-500 totalCards right-24">
-          {!flashCardData.length ? null : `Total FlashCards : ${flashCardData.length}`}
+          {!flashCardData.length
+            ? null
+            : `Total FlashCards : ${flashCardData.length}`}
         </div>
-        <div name="displayFlashcardDiv" className="flex flex-wrap m-auto overflow-hidden ">
+        {/* Display the flashcards */}
+        <div
+          name="displayFlashcardDiv"
+          className="flex flex-wrap m-auto overflow-hidden "
+        >
           {flashCardData.length !== 0 ? (
             flashCardData.slice(0, showCard).map((elem, index) => (
               <div
@@ -50,6 +70,7 @@ const MyFlashcardsPage = () => {
                 name="childCards"
                 className="commonBorder childCards flex flex-col m-auto bg-white w-[300px] h-[200px] p-[8px] rounded mt-[50px] relative mb-[10px] "
               >
+                {/* Delete button for each flashcard*/}
                 <button
                   className="absolute hidden text-3xl text-gray-500 del -right-3 -top-5 hover:text-4xl hover:text-red-600 "
                   onClick={() => {
@@ -58,6 +79,7 @@ const MyFlashcardsPage = () => {
                 >
                   <GiCrossMark />
                 </button>
+                {/* Group image or default image*/}
                 <img
                   className="border-2 bg-slate-400 w-[70px] h-[70px] m-auto rounded-full absolute -top-12 left-[39.3%] mb-10"
                   src={elem.groupImage ? elem.groupImage : almabetter}
@@ -65,9 +87,13 @@ const MyFlashcardsPage = () => {
                 />
                 <h1 className="mt-4 font-bold ">{elem.groupName}</h1>
                 <h2 className="h-10 mt-1 text-gray-700">
-                  {elem.groupDescription.length > 60 ? elem.groupDescription.slice(0, 60) + "..." : elem.groupDescription}
+                  {elem.groupDescription.length > 60
+                    ? elem.groupDescription.slice(0, 60) + "..."
+                    : elem.groupDescription}
                 </h2>
-                <h2 className="mt-8 font-bold text-gray-500">{elem.term.length} Cards</h2>
+                <h2 className="mt-8 font-bold text-gray-500">
+                  {elem.term.length} Cards
+                </h2>
                 <button
                   className="w-40 h-8 m-auto font-medium text-red-600 duration-300 border-2 border-red-500 rounded hover:bg-red-500 hover:text-white"
                   onClick={() => handleViewCardsClick(elem)}
@@ -77,9 +103,16 @@ const MyFlashcardsPage = () => {
               </div>
             ))
           ) : (
+            // display when there are no flascards.
             <div className="w-[100%] h-[80vh] rounded noFlashcard overflow-hidden relative font-bold">
-              <img className="absolute w-[100%] h-[100%]" src={noFlashCard} alt="" />
-              <div className="mt-32 text-red-800 text-7xl backdrop-blur-sm">"No Flashcard available"</div>
+              <img
+                className="absolute w-[100%] h-[100%]"
+                src={noFlashCard}
+                alt=""
+              />
+              <div className="mt-32 text-red-800 text-7xl backdrop-blur-sm">
+                "No Flashcard available"
+              </div>
               <br />
               <p className="mt-5 text-xl backdrop-blur-sm">
                 Please go and
@@ -89,6 +122,7 @@ const MyFlashcardsPage = () => {
               </p>
             </div>
           )}
+          {/* Conditionally render buttons to show more or fewer flashcards */}
           {flashCardData && flashCardData.length > 6 ? (
             <div className="w-[100%]">
               <div className="mt-5 text-right">
